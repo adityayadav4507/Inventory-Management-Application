f8fabfae71db9ce840dad30c5acd52c4bf612f8b
Aditya Yadav <aditya.yadav.min22@itbhu.ac.in>
2026-06-01 21:16:53 +0530

    Fix production frontend CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://your-vercel-app.vercel.app"
+    "https://inventory-management-application-two.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
