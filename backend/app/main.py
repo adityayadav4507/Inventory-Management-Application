9e5a23f29de60a7e1fcf48fbb8c2ae443f326d34
Aditya Yadav <aditya.yadav.min22@itbhu.ac.in>
2026-06-01 17:41:59 +0530

    CORSMiddleware in database.py in backend folder changes to the allow_origins to vercel link of the frontend deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,10 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://your-vercel-app.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
